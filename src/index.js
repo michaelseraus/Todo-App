c178db018481c090c0d3ae8b354c614c3eaa8ad6
Michael <mseraus@gmail.com>
2020-11-26 12:34:30 +0100

    styling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./Styles/app.scss";
 import App from "./App";
 import "react-calendar/dist/Calendar.css";
-import "./Styles/app.scss";
 
 ReactDOM.render(
   <React.StrictMode>
